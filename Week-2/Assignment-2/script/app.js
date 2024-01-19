c0735db9768f6113690bb7342ef71a6898d68b5c
Lily Chou <lily28252005@gmail.com>
2024-01-19 20:32:52 +0800

    Week2 version2

--- Week-2/Assignment-2/script/app.js
+++ Week-2/Assignment-2/script/app.js
@@ -1,8 +1,8 @@
-var welcome = document.querySelector('.message');
-var message = document.querySelector('.message div');
-var action = document.querySelector('.action');
-var show = document.getElementsByClassName('.show');
-var boxContainer2 = document.querySelector('.box-container2')
+let welcome = document.querySelector('.message');
+let message = document.querySelector('.message div');
+let action = document.querySelector('.action');
+let show = document.getElementsByClassName('.show');
+let boxContainer2 = document.querySelector('.box-container2')
 welcome.addEventListener('click', () => {
     welcome.textContent = "Have a Good Time!";
 });
