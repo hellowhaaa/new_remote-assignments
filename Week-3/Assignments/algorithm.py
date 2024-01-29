7b9e92b60ee0154187029454b3235da8607f849a
Lily Chou <lily28252005@gmail.com>
2024-01-29 21:26:58 +0800

    Week Assignments version4

--- Week-3/Assignments/algorithm.py
+++ Week-3/Assignments/algorithm.py
@@ -1,10 +1,9 @@
-
 def two_sum(nums, target):
+    dic = {}
     for i in range(len(nums)):
-        other_number = target - nums[i]
-        if other_number in nums:
-            other_index = nums.index(other_number)
-            return [i, other_index]
+        if target-nums[i] in dic:
+            return [i, dic[target-nums[i]]]
+        dic[nums[i]] = i
 
 
 ans = two_sum([2, 7, 11, 15], 9)
--- Week-3/Assignments/static/script.js
+++ Week-3/Assignments/static/script.js
@@ -1,39 +1,29 @@
-
 (function () {
-    var xhr;
-    document
-        .getElementById("get-btn")
-        .addEventListener("click", makeRequest);
-    function makeRequest() {
-        var number = document.getElementById('text').value
-        console.log(number)
-        xhr = new XMLHttpRequest();
-        if (!xhr) {
-            alert("Giving up :( Cannot create an XMLHTTP instance");
-            return false;
-        }
-        xhr.onreadystatechange = alertContents;
-        xhr.open('GET', 'http://127.0.0.1:3000/data?number=' + number, true);
-        xhr.send();
-
-        function alertContents() {
-            try {
-                if (xhr.readyState === XMLHttpRequest.DONE) {
-                    if (xhr.status === 200) {
-                        alert(xhr.responseText);
-                    } else {
-                        alert("There was a problem with the request.");
-                    }
-                }
-            }
-            catch (e) {
-                alert("Caught Exception: " + e.description);
-            }
-        }
+  let xhr;
+  document.getElementById("get-btn").addEventListener("click", makeRequest);
+  function makeRequest() {
+    let number = document.getElementById("text").value;
+    console.log(number);
+    xhr = new XMLHttpRequest();
+    if (!xhr) {
+      alert("Giving up :( Cannot create an XMLHTTP instance");
+      return false;
     }
-}
-)();
-
-
-
-
+    xhr.onreadystatechange = alertContents;
+    xhr.open("GET", "http://127.0.0.1:3000/data?number=" + number, true);
+    xhr.send();
+    function alertContents() {
+      try {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+          if (xhr.status === 200) {
+            alert(xhr.responseText);
+          } else {
+            alert("There was a problem with the request.");
+          }
+        }
+      } catch (e) {
+        alert("Caught Exception: " + e.description);
+      }
+    }
+  }
+})();
